7363302505c26b2317d435704b60f56cd3a3b38d
dennisdevetter <dennisdevetter@gmail.com>
2023-01-13 16:02:08 +0100

    change state

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,9 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0.0;
 
 contract HelloWorld {
-    uint public value;
+    uint private value;
 
     function setValue(uint newValue) public {
         value = newValue;
+    }
+
+    function getValue() public view returns (uint) {
+        return value;
+    }
+
+    function remove() public {
+        selfdestruct(payable(address(0x0)));
     }
 }
