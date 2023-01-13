33afc67c7fd5e3b62ffaf159b235c73f3dcb3ba2
dennisdevetter <dennisdevetter@gmail.com>
2023-01-13 19:34:14 +0100

    vote via string

--- Voter.sol
+++ Voter.sol
@@ -5,6 +5,7 @@ contract Voter {
     
     uint[] public votes;
     string[] public options;
+    mapping(address => bool) private hasVoted;
 
     constructor(string[] memory _options) {
         require(options.length == 0, "options required");
@@ -14,6 +15,30 @@ contract Voter {
 
     function vote(uint option) public {
         require(option < options.length, "Invalid option");
+        require(!hasVoted[msg.sender], "Already voted");
+        recordVote(option);
+    }
+
+    function vote(string memory option) public {
+        require(!hasVoted[msg.sender], "Already voted");
+
+        for(uint i = 0; i < options.length; i++) {
+            string memory currOption = options[i];
+            if (stringEqual(option, currOption)) {
+                recordVote(i);
+                return;
+            } 
+        }
+
+        revert();
+    }
+
+    function stringEqual(string memory a, string memory b) private pure returns (bool) {
+        return keccak256(bytes(a)) == keccak256(bytes(b));
+    }
+
+    function recordVote(uint option) private {
+        hasVoted[msg.sender] = true;
         votes[option] = votes[option] + 1;
     }
     
